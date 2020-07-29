e0e9037da05cdef3297de8403e50dad1d5a58054
LeeYishan <K_attitude@LeeYishandeMacBook-Air.local>
2020-07-29 10:34:26 +0800

    猜數字

--- c_to_f.py
+++ c_to_f.py
@@ -1,5 +1,17 @@
-temperature = input("現在攝氏溫度是: ")
-temperature = int(temperature)
+import random
 
-fahrenheit = temperature * 9/5 + 32
-print('華氏溫度為：', fahrenheit)
+r = random.randint(1, 100)
+
+
+while True:
+	num = input("請猜數字: ")
+	num = int(num)
+	print("正確答案是：", r)
+	if r == num:
+		print("終於答對了")
+		break
+	elif num > r:
+			print("你猜的數字比答案大")
+	elif num < r:
+			print("你猜的數字比答案小")
+

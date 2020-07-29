7dba673bc02979eafc2648d6dbc3db5931273cfc
LeeYishan <K_attitude@LeeYishandeMacBook-Air.local>
2020-07-29 14:32:43 +0800

    review_analytics_update_again

--- review_analytics.py
+++ review_analytics.py
@@ -9,6 +9,7 @@ with open('reviews.txt', 'r') as f:
 			print(len(data))
 print("檔案讀取完畢，總共有 ", len(data), " 筆資料")
 
+#刪選留言平均長度小於 100 的
 sum_len = 0
 for d in data:
 	sum_len = sum_len + len(d)
@@ -27,3 +28,19 @@ for d in data:
 	if len(d) < 100:
 		new.append(d)
 print("一共有 ", len(new), " 筆留言小於 100")
+
+
+#搜尋留言裡面有 "good" 字串
+good_count = 0
+for d in data:
+	if "good" in d:
+		good_count +=1
+print("有 good 的留言總共有 ", good_count, " 筆")
+
+#或是以下寫法
+good = []
+for d in data:
+	if "good" in d:
+		good.append(d)
+print("總共有 ", len(good), " 筆小於 100 字")
+

5e50af379c33462c19ef7c4101d06f18d1f4dcab
LeeYishan <K_attitude@LeeYishandeMacBook-Air.local>
2020-07-29 14:25:06 +0800

    review_analytics_update

--- review_analytics.py
+++ review_analytics.py
@@ -5,7 +5,7 @@ with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
 		count += 1
-		if count % 1000 == 0:
+		if count % 100000 == 0:
 			print(len(data))
 print("檔案讀取完畢，總共有 ", len(data), " 筆資料")
 
@@ -13,3 +13,17 @@ sum_len = 0
 for d in data:
 	sum_len = sum_len + len(d)
 print("留言平均長度是 ", sum_len/len(data))
+
+count = 0
+for d in data:
+	if len(d) < 100: 
+		count += 1
+print("總共有 ", count, " 筆小於100")
+
+#或是以下寫法
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print("一共有 ", len(new), " 筆留言小於 100")

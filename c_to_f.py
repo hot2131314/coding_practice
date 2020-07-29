30f0191920735e81852ae24b63c103e18887e732
LeeYishan <K_attitude@LeeYishandeMacBook-Air.local>
2020-07-29 10:52:46 +0800

    猜數字update

--- c_to_f.py
+++ c_to_f.py
@@ -1,17 +1,21 @@
 import random
 
 r = random.randint(1, 100)
-
+count = 0
 
 while True:
+	count +=  1
+
 	num = input("請猜數字: ")
 	num = int(num)
 	print("正確答案是：", r)
 	if r == num:
 		print("終於答對了")
+		print("你總共猜了 ", count, " 次！")
 		break
 	elif num > r:
 			print("你猜的數字比答案大")
 	elif num < r:
 			print("你猜的數字比答案小")
+	print("你總共猜了 ", count, " 次！")
 
